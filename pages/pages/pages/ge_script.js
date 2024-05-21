bf05121a678956ea4b345a26d0f3cb334c12f254
Gabriel Duarte <gabrielgoncalvesduarte5@gmail.com>
2024-05-21 10:56:44 -0300

    Criação da Sub Página dos Cursos

---
+++ pages/pages/pages/ge_script.js
@@ -0,0 +1,26 @@
+function toggleSubMenu(id) {
+    var submenu = document.getElementById(id);
+    if (submenu.style.display === "none") {
+        submenu.style.display = "block";
+    } else {
+        submenu.style.display = "none";
+    }
+}
+function toggleSubSubMenu(id) {
+    var submenu = document.getElementById(id);
+    if (submenu.style.display === "none") {
+        submenu.style.display = "block";
+    } else {
+        submenu.style.display = "none";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var paragraphs = document.querySelectorAll("#textAnimation p");
+  
+    paragraphs.forEach(function(paragraph, index) {
+      setTimeout(function() {
+        paragraph.style.opacity = 1;
+      }, index * 5000); // Delay each paragraph by 1 second
+    });
+  });
